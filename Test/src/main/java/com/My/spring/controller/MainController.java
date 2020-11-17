d6deb52256de806069856f0c786bdb366c085867
your_email@example.com <wjsdudqo70@naver.com>
2020-11-17 17:34:55 +0900

    First

---
+++ Test/src/main/java/com/My/spring/controller/MainController.java
@@ -0,0 +1,40 @@
+package com.My.spring.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.Myapp.test.HomeController;
+
+@Controller
+public class MainController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String main(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/board")
+	public String test(Model model) {
+		
+		return "board";
+	}
+	
+}
---
+++ Test/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,12 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<%@ page language="java" pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+	<a href="board">게시판</a>
+</body>
+</html>
